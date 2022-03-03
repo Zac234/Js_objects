14842036200c18c38c55f6c5301b504805702f8f
zac234 <zacbenincasa@gamil.com>
2022-03-03 12:54:11 -0700

    this works on my laptop

---
+++ app.js
@@ -0,0 +1,57 @@
+var tweets =[{
+    tweet:"this is gonna be great",
+    user:"harambe",
+    created_at:28/05/2016,
+    age:17,
+
+    tweet:"awesome",
+    user:"ZAc123",
+    created_at:10/09/2018,
+    age:18,
+
+    tweet:"jokes",
+    user:"nero",
+    created_at:12/08/14,
+    age:45,
+
+    tweet:"goodbye",
+    user:"caesar",
+    created_at:04/04/19,
+    age:35,
+
+    tweet:"what are you gonna do about it",
+    user:"Brutus",
+    created_at:05/07/20,
+    age:27,
+
+    tweet:"hello",
+    user:"Grecko",
+    created_at:09/02/14,
+    age:22,
+
+    tweet:"we are trying to reach you about your car insurance",
+    user:"Intact354",
+    created_at:04/12/20,
+    age:49,
+
+    tweet:"reach out",
+    user:"BaronFlynt253",
+    created_at:02/27/17,
+    age:62,
+
+    tweet:"whats up it's your boy HP, back with the fish people",
+    user:"Chthulu716",
+    created_at:01/04/21,
+    age:34,
+
+    tweet:"you can be who you want",
+    user:"Mob99",
+    created_at:09/09/19,
+    age:19
+}]
+    
+console.log(tweets.age)
+var aged_tweets = tweets.filter(age=>18)
+
+
+
